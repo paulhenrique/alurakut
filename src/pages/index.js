7440d4dcb1d070d987b28f8227e15842da118cb0
Paulo Henrique <phvcandido@gmail.com>
2021-07-12 19:29:26 -0300

    Iniciando estilização da página inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,17 @@ import Container from '../components/Container';
 
 const Box = styled.div`
   background:#ffffff;
+  padding:10px;
+  border-radius:5px;
 `;
 
 const Main = styled.main`
+  max-width:1110px;
+  width:100%;
+  margin: 0 auto;
   display:grid;
   grid-gap:10px;
-  padding:16px;
+  padding-top:10px;
   @media(min-width:860px){
     grid-template-areas:"profileArea welcomeArea profileRelationsArea";
     grid-template-columns:160px 1fr 312px;
@@ -17,16 +22,16 @@ const Main = styled.main`
 `;
 
 export default function Home() {
+  const user = 'paulhenrique';
   return (
     <>
       <Navbar />
       <Main>
-        <Container>
-          <img src="https://github.com/paulhenrique.png" alt="Paulo" />
-          <Box style={{ gridArea: 'profileArea' }}>Imagem</Box>
-          <Box style={{ gridArea: 'welcomeArea' }}>Bem vindo</Box>
-          <Box style={{ gridArea: 'profileRelationsArea' }}>Comunidades</Box>
-        </Container>
+        <Box style={{ gridArea: 'profileArea' }}>
+          <img style={{ width: '100%' }} src={`https://github.com/${user}.png`} alt="Paulo" />
+        </Box>
+        <Box style={{ gridArea: 'welcomeArea' }}>Bem vindo</Box>
+        <Box style={{ gridArea: 'profileRelationsArea' }}>Comunidades</Box>
       </Main>
     </>
   );
